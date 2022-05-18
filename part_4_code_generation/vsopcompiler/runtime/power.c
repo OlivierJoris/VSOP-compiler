5c860cf2e0f97b12c303e44795b6d433f12465a8
Olivier Joris <joris.olivier@hotmail.com>
2022-05-18 17:56:07 +0200

    Handle case n^0 in power function

--- part_4_code_generation/vsopcompiler/runtime/power.c
+++ part_4_code_generation/vsopcompiler/runtime/power.c
@@ -4,8 +4,15 @@ int64_t power(int32_t n, int32_t p)
 {
     int64_t result = n;
     
-    for(unsigned int i = 0; i <= p; ++i)
-    	result *= n;
+    if(p == 0)
+    {
+        return 1;
+    }
+
+    for(int64_t i = 1; i < p; i++)
+    {
+        result *= n;
+    }
     	
     return result;
 }
